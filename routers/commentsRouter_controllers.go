c6b42348b66db97f5bdf310315dedbdbf7cf04bb
DuanEnJian <backtrack843@163.com>
2018-01-04 10:47:39 +0800

    init project

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,98 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["12306/controllers:ObjectController"] = append(beego.GlobalControllerRouter["12306/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:ObjectController"] = append(beego.GlobalControllerRouter["12306/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:ObjectController"] = append(beego.GlobalControllerRouter["12306/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method: "Get",
+			Router: `/:objectId`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:ObjectController"] = append(beego.GlobalControllerRouter["12306/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:objectId`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:ObjectController"] = append(beego.GlobalControllerRouter["12306/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:objectId`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:UserController"] = append(beego.GlobalControllerRouter["12306/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:UserController"] = append(beego.GlobalControllerRouter["12306/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:UserController"] = append(beego.GlobalControllerRouter["12306/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Get",
+			Router: `/:uid`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:UserController"] = append(beego.GlobalControllerRouter["12306/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:uid`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:UserController"] = append(beego.GlobalControllerRouter["12306/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:uid`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["12306/controllers:UserController"] = append(beego.GlobalControllerRouter["12306/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Logout",
+			Router: `/logout`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
---
+++ routers/router.go
@@ -0,0 +1,58 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"net/http"
+	"12306/controllers"
+	"strings"
+	"12306/utils"
+	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+		beego.NSRouter("/auth/login",&controllers.UserController{},"Post:Login"),
+		beego.NSNamespace("/user",
+			beego.NSBefore(Auth),
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
+
+func Auth(ctx *context.Context){
+	authString := ctx.Input.Header("Authorization")
+    beego.Debug("AuthString:", authString)
+	if authString == ""{
+		ctx.Output.Header("Cache-Control","no-store")
+		ctx.Output.Header("WWW-Authenticate","Bearer realm=\""+beego.AppConfig.String("HostName")+"\" error=\"Authorization\" error_description=\"invalid Authorization\"")
+		http.Error(ctx.ResponseWriter, "Unauthorized", 401)
+		return
+	}
+    kv := strings.Split(authString, " ")
+    if len(kv) != 2 || kv[0] != "Bearer" {
+		ctx.Output.Header("Cache-Control","no-store")
+		ctx.Output.Header("WWW-Authenticate","Bearer realm=\""+beego.AppConfig.String("HostName")+"\" error=\"Authorization\" error_description=\"invalid Authorization\"")
+		http.Error(ctx.ResponseWriter, "Unauthorized", 401)
+		return
+    }
+	token := kv[1]
+	beego.Debug(token)
+	jwt := &utils.Jwt{}
+	jwt.SetSecretKey(beego.AppConfig.String("JwtKey"))
+	if !jwt.Checkd(token) {
+		ctx.Output.Header("Cache-Control","no-store")
+		ctx.Output.Header("WWW-Authenticate","Bearer realm=\""+beego.AppConfig.String("HostName")+"\" error=\"Authorization\" error_description=\"invalid Authorization\"")
+		http.Error(ctx.ResponseWriter, "Unauthorized", 401)
+		return
+	}
+}
